da0ff1bdedce9f56b32b0509063ab206504899ce
Isabella <angelcuartas18@gmail.com>
2024-10-09 07:26:44 -0500

    Funciones base

--- Server/server.js
+++ Server/server.js
@@ -1,3 +1,5 @@
+// USUARIO ESTUDIANTE
+
 // Esta función obtiene información de restaurantes
 function getListStores (){
 
@@ -10,5 +12,17 @@ function bringData(){
 
 //Esta función verifica la información del almuerzo escogido
 function checkInfo (){
+
+}
+
+//ADMINISTRADOR DE CAFETERIA
+
+//Guarda la información del formulario de subir almuerzo
+function saveLunch (){
+
+}
+
+//Verifica el usuario del adminustrador
+function getuser (){
 
 }
